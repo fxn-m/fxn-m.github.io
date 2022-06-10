e0aaadc32d84ebc0b390eee5ef88b552b0eb353e
Felix Newport-Mangell <felixnewportmangell98@gmail.com>
2022-06-11 00:57:44 +0100

    ...

---
+++ story_generator.js
@@ -0,0 +1,25 @@
+// a program to write random 3-part stories in javascript
+
+// each story follows the same general principle
+// beginnings set a scene, for which some action will take place
+// middles include a description of the action
+// endings describe the result of the action
+
+// beginnings
+
+
+const beginnings = ["The performance opened with a traditional display, ",
+                    "Jimmy's first day at school began unexpectedly when after opening his form-room door he saw what appeared to be ",
+                    "Emily jumped off the boat and believed she was picking up the familiar scent of "] ; 
+
+const middles = ["twelve micro-piglets playing texas hold-em. ",
+                 "a brigade of bandits, braiding eachother's hair. ",
+                 "Gordon Ramsey's lobster thermidor. "]
+
+const endings = ["It was most off-putting.",
+                 "No-one was amused.",
+                 "Close-by, a boy murmured: 'I like trains'."]
+
+
+idx = 2
+console.log(beginnings[idx] + middles[idx] + endings[idx]);
