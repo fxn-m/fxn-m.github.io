a912520923c78123c7af2fdb85d126a9c559ce31
Felix Newport-Mangell <felixnewportmangell98@gmail.com>
2022-06-13 23:24:18 +0100

    add color transition for day clock

--- day_clock.js
+++ day_clock.js
@@ -1,11 +1,18 @@
-const tZero = 1655146653478 - 86400000*3;
+const tZero = 1655146600000;
 const msInDay = 24 * 60 * 60 * 1000;
 let day = Math.floor( (new Date().getTime() - tZero) / msInDay );
 
 let clockP = document.getElementById('day-clock');
 
 function updateDayClock() {
-    clockP.innerHTML = 'day ' + String(day);
+    clockP.innerHTML = String(day);
+}
+
+function colorChange() {
+    clockP.style.color = '#' + Math.random().toString(16).slice(-6);
 }
 
 window.onload = updateDayClock;
+
+// setInterval(setTimeOut(colorChange, 100), 1000)
+setInterval(colorChange, 500)
