792e864c5793743825757544ae0e1cc109fc2034
Felix Newport-Mangell <felixnewportmangell98@gmail.com>
2022-06-13 23:42:40 +0100

    add date faking code

--- day_clock.js
+++ day_clock.js
@@ -1,10 +1,10 @@
 const tZero = 1655146600000;
 const msInDay = 24 * 60 * 60 * 1000;
-let day = Math.floor( (new Date().getTime() - tZero) / msInDay );
 
 let clockP = document.getElementById('day-clock');
 
 function updateDayClock() {
+    let day = Math.floor( (new Date().getTime() - tZero) / msInDay );
     clockP.innerHTML = String(day);
 }
 
@@ -13,6 +13,15 @@ function colorChange() {
 }
 
 window.onload = updateDayClock;
-
-// setInterval(setTimeOut(colorChange, 100), 1000)
 setInterval(colorChange, 500)
+
+
+// fake the date
+var fake_date = new Date("November 30, 2022 06:00:00");
+
+// overriding date function
+Date = function(){return fake_date;};
+var new_date = new Date();
+alert(new_date);
+
+setInterval(updateDayClock, 100)
