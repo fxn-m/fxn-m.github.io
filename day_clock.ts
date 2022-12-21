766ed9b3c9ed45d67bed9ac4c3f0804f0af36671
Felix Newport-Mangell <felixnewportmangell98@gmail.com>
2022-12-21 01:27:36 +0000

    add TypeScript

--- day_clock.ts
+++ day_clock.ts
@@ -1,20 +1,27 @@
-"use strict";
 const tZero = 1655146600000;
 const msInDay = 24 * 60 * 60 * 1000;
+
 const clockP = document.getElementById('day-clock');
+
 function updateDayClock() {
-    let day = Math.floor((new Date().getTime() - tZero) / msInDay);
-    clockP.innerHTML = String(day);
+    let day = Math.floor( (new Date().getTime() - tZero) / msInDay );
+    clockP!.innerHTML = String(day);
 }
+
 function colorChange() {
-    clockP.style.color = '#' + Math.random().toString(16).slice(-6);
+    clockP!.style.color = '#' + Math.random().toString(16).slice(-6);
 }
+
 window.onload = updateDayClock;
-setInterval(colorChange, 500);
+setInterval(colorChange, 500)
+
+
 // // fake the date
 // var fake_date = new Date("November 30, 2022 06:00:00");
+
 // // overriding date function
 // Date = function(){return fake_date;};
 // var new_date = new Date();
 // alert(new_date);
+
 // setInterval(updateDayClock, 100)
