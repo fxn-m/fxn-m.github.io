be1ae5450de6e9befde77d9e60cdc7008874a173
Felix Newport-Mangell <felixnewportmangell98@gmail.com>
2023-01-09 21:08:18 +0000

    print-html working better

--- scripts/print-html.js
+++ scripts/print-html.js
@@ -1,13 +1,22 @@
 let output = ''
 
 const print = () => {
-    let html = document.getElementById('url').value
-    console.log(html)
+    let url = document.getElementById('url').value
+    console.log(url)
 
-    fetch(html)
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById('print-html-output').innerHTML = data;
+    fetch('https://sheltered-everglades-04891.herokuapp.com/' + url, {
+        method: 'GET'
     })
-    .catch(error => console.log(error))
+    .then(response => {
+        console.log(response);
+        return response.text();
+    })
+    .then(data => {
+        console.log(data)
+        const escaped = data.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;')
+        document.getElementById('print-html-output').innerHTML = escaped;
+    })
+    .catch(error => {
+        console.log(error);
+        document.getElementById('print-html-output').innerHTML = error})
 }
