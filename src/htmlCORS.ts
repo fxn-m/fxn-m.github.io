174c60257a6d15a1411322ffd6eac4989ae23535
Felix Newport-Mangell <felixnewportmangell98@gmail.com>
2023-03-10 20:42:18 +0000

    reboot pt.2

---
+++ src/htmlCORS.ts
@@ -0,0 +1,56 @@
+const setupRender = () => {
+
+let url: any
+let timer: any
+const typeWriter = (data: any) => {
+    let output: any = document.getElementById('print-html-output')
+    if (timer) {
+        clearInterval(timer)
+    }
+    output.innerHTML = '\n'
+    let i = 0
+    timer = setInterval(() => {
+        if (i < data.length) {
+            output.innerHTML += data.charAt(i);
+            i++
+        } else {
+            clearInterval(timer)
+        }
+    }, .5)
+}
+
+const render = () => {
+    url = document.getElementById('url')
+
+    fetch('https://sheltered-everglades-04891.herokuapp.com/' + url.value, {
+        method: 'GET'
+    })
+        .then(response => {
+            return response.text();
+        })
+        .then(data => {
+            typeWriter(data)
+        })
+        .catch(error => {
+            clearInterval(timer)
+            document.getElementById('print-html-output')!.innerHTML = '\n' + error
+        })
+}
+
+const foo = (element: HTMLButtonElement) => {
+    element.addEventListener('click', render)
+}
+
+const bar = (element: HTMLInputElement) => {
+    element.addEventListener('keydown', (e) => {
+        console.log(e)
+        if (e.code == "Enter") render()
+    })
+}
+
+foo(document.querySelector('#submit')!);
+bar(document.querySelector('#url')!);
+
+} 
+
+export default setupRender
---
+++ src/main.ts
@@ -0,0 +1,44 @@
+import setupRender from './htmlCORS'
+
+const anyhtml = () => {
+  document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <h3>
+    AnyHTML
+  </h3>
+  <p>
+    Input any website url and see the corresponding page's HTML below:
+  </p>
+
+  <div style="display: flex;">
+  <input type="url" id="url" name="url" placeholder='https://example.com' value='https://example.com' style='margin-right:10px'><br><br>
+  <button id="submit">
+    Submit
+  </button>
+  </div>
+
+  <br>
+  
+  <pre style="
+    overflow-x: auto;
+    white-space: pre-wrap;
+    white-space: -moz-pre-wrap;
+    white-space: -pre-wrap;
+    white-space: -o-pre-wrap;
+    word-wrap: break-word;">
+      <code id="print-html-output">
+      </code>
+  </pre>
+  `
+}
+
+const showAnyhtml = (element: HTMLAnchorElement) => {
+  element.addEventListener('click', () => {
+    anyhtml();
+    setupRender();
+    });
+  
+}
+
+showAnyhtml(document.querySelector('#anyhtml')!);
+
+export {}
