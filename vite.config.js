df5f15769ec3d57d14bebab9e2f44caf5818f138
Felix Newport-Mangell <felixnewportmangell98@gmail.com>
2023-03-11 02:01:09 +0000

    move index.html out of src

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import path from 'path'
 
 export default {
-    base: 'https://fxn-m.com/',
-    root: path.resolve(__dirname, 'src'),
+    // root: path.resolve(__dirname, 'src'),
     resolve: {
         alias: {
             '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
