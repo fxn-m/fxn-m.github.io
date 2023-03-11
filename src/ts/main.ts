27404acbb053f4b7db8cf950685922883261cebc
Felix Newport-Mangell <felixnewportmangell98@gmail.com>
2023-03-11 00:59:05 +0000

    working with bootstrap and scss

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,4 +1,9 @@
-import './styles.css';
+// Import our custom CSS
+import '../scss/styles.scss'
+
+// Import all of Bootstrap's JS
+// import * as bootstrap from 'bootstrap'
+
 import setupRender from './htmlCORS'
 
 const anyhtml = () => {
