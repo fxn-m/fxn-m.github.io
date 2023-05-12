134729e09ecc2f86e1f3320b154ad1205581ba37
Felix Newport-Mangell <felixnewportmangell98@gmail.com>
2023-05-12 01:47:16 +0100

    toggle dark mode added

---
+++ src/ts/toggle-theme.ts
@@ -0,0 +1,36 @@
+let toggleBtn = document.getElementById("toggle-btn");
+
+function setTheme(isDark: boolean) {
+  if (isDark) {
+    localStorage.setItem("theme", "dark");
+    document.body.classList.add("dark");
+    toggleBtn!.innerHTML = "☀️";
+    toggleBtn!.setAttribute("aria-label", "enable light theme");
+  } else {
+    localStorage.setItem("theme", "light");
+    document.body.classList.remove("dark");
+    toggleBtn!.innerHTML = "🌙";
+    toggleBtn!.setAttribute("aria-label", "enable dark theme");
+  }
+}
+
+function toggleTheme() {
+  if (localStorage.getItem("theme") === "dark") {
+    setTheme(false);
+  } else {
+    setTheme(true);
+  }
+}
+
+if (!localStorage.getItem("theme")) {
+  const osTheme = window.matchMedia("(prefers-color-scheme: dark)").matches
+    ? "dark"
+    : "light";
+  localStorage.setItem("theme", osTheme);
+}
+
+toggleBtn!.addEventListener("click", (e) => {
+  e.preventDefault();
+  toggleTheme();
+});
+setTheme(localStorage.getItem("theme") === "dark");
