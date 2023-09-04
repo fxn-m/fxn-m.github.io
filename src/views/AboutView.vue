e2fa05004f13414bf36fb213383fee5933c3ee48
felix <fnewportmangell@gmail.com>
2023-09-04 21:34:21 +0100

    update about info

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -4,13 +4,14 @@
 <template>
     <div id="content">
         <p>Travelling, and working on side-quests.</p>
-        <p>Currently: near Barcelona.</p>
+        <p>Currently: near Cambridge.</p>
         <h3 id="i-m-a-software-design-engineer-">I&#39;m a Software &amp;&amp; Design Engineer.</h3>
-        <p>Engineering Design at the University of Bristol, the liberal arts of engineering. </p>
+        <p>Studied Engineering Design at the University of Bristol, the 'liberal arts of Engineering'.</p>
         <p>I worked on projects across a bunch of fields; from operations research to computer vision. My research
             looked at automating lithium-ion battery disassembly and re-purposing.</p>
-        <p>Self-taught software engineer. Started in data science & ML projects, now going for full-stack.</p>
-        <p>Based in Cambridge, for now. In 2024 I'm moving to London for the foreseeable future.</p>
+        <p>Over 5 years I spent 18 months in industry. I spent a year designing and building <a href="https://www.babcockinternational.com/case-study/whls/" target="_blank">submarine systems</a>, and did research and development on <a href="https://careers.dyson.com/en-gb/what-you-can-do/engineer/energy-storage-batteries/" target="_blank">lithium-ion batteries</a>.</p>
+        <p>At some point I picked up programming, starting with data science & ML projects. I'm self-taught and now learning full-stack engineering, building better patient experiences @ <a href="https://www.sanctuaryhealth.io/" target="_blank">Sanctuary Health</a>.</p>
+        <p>Based in Cambridge, for now. In 2024 I'll be in London.</p>
         <h3 id="experience">Experience</h3>
         <ul>
             <li>2022 Technical Research Intern @ Dyson (3 months)</li>
@@ -25,7 +26,7 @@
             <li><strong>Reading.</strong> I'm curious about almost everything. Sometimes I remember to <a
                     href="https://www.goodreads.com/user/show/111634482-felix" target="_blank">record what I've
                     read</a>.</li>
-            <li><strong>Running.</strong> I ran my first half marathon in May, and loved it. Now training for a sub
+            <li><strong>Running.</strong> I ran my first half marathon in May. Enjoyed it so much I'm now training for a sub
                 20:00 5k in October. <a href="https://www.strava.com/athletes/29743058" target="_blank">See my
                     progress</a>.</li>
             <li><strong>Music.</strong> I <a href="https://stats.fm/fnm" target="_blank">listen</a> to a lot of
