56e8c275c5c1b5e987efd5d7f80528e049b984df
fxn-m <felixnewportmangell98@gmail.com>
2023-09-17 19:53:23 +0100

    tidying up

--- src/views/Fun/FunsView.vue
+++ src/views/Fun/FunsView.vue
@@ -5,14 +5,12 @@
   <div id="content">
     <ul>
       <li>
+        <a href="https://www.fxn-m.com/atmosphere.fm" target="_blank">atmosphere.fm</a>
+      </li>
+      <li>
         <RouterLink to="/fun/:anyhtml">AnyHTML</RouterLink>
       </li>
-      <li>
-        <!-- <RouterLink to="/work">Work</RouterLink> -->
-      </li>
-      <li>
-        <!-- <RouterLink to="/essays">Essays</RouterLink> -->
-      </li>
+
     </ul>
   </div>
 </template>
