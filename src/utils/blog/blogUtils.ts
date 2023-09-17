8d5bcfbd024ca7b82254742cc28d76951d39ba28
fxn-m <felixnewportmangell98@gmail.com>
2023-09-17 20:08:44 +0100

    blogs live!

--- src/utils/blog/blogUtils.ts
+++ src/utils/blog/blogUtils.ts
@@ -2,7 +2,6 @@ import axios from "axios";
 
 export const getBlogPosts = async () => {
   const k = import.meta.env.VITE_STRAPI_API_KEY;
-  console.log(k);
 
   const posts = await axios.get(
     "https://fxnm-strapi-blog-9e2c8c1b9091.herokuapp.com/api/articles",
