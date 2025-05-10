10c65ccc325d8b3e19bda3c8ab8e7f5f5f05a2ab
felix <67316355+fxn-m@users.noreply.github.com>
2025-05-10 21:50:40 +0100

    add tanstack query

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,15 @@ import "./main.css"
 import { faArrowLeft, faArrowsRotate, faCaretLeft, faCircleQuestion, faEnvelope, faFlagCheckered, faLaptop } from "@fortawesome/free-solid-svg-icons"
 import { faCircleXmark, faCompass } from "@fortawesome/free-regular-svg-icons"
 import { faGithub, faStrava, faXTwitter } from "@fortawesome/free-brands-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome"
+import { library } from "@fortawesome/fontawesome-svg-core"
 
 import App from "./App.vue"
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome"
-import { createApp } from "vue"
-import { library } from "@fortawesome/fontawesome-svg-core"
 import router from "./router"
+import { createApp } from "vue"
+import { QueryClient, VueQueryPlugin } from "@tanstack/vue-query"
+
+const queryClient = new QueryClient()
 
 library.add(
   faArrowLeft,
@@ -26,6 +29,10 @@ library.add(
 )
 
 const app = createApp(App)
+
+app.use(VueQueryPlugin, {
+  queryClient
+})
 
 app.use(router)
 app.component("font-awesome-icon", FontAwesomeIcon)
